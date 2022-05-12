74356b768d8d05baaf018f221968a1ea8348c889
GOKUL PRSASATH <83939754+gokul299@users.noreply.github.com>
2022-05-12 11:08:01 +0530

    Update script.js

--- Tree Reply - Comment/script.js
+++ Tree Reply - Comment/script.js
@@ -1,70 +1,51 @@
-const template = document.createElement("template");
-template.innerHTML = ` <link rel="stylesheet" href="./style.css" />
-        <div class="comment-box">
-        <div class="comment-edit">
-            <input class="comment-input" type="text" placeholder=' Type Something....'/>
-            <button class="submit-btn">Reply</button>
-        </div>
-        <div class="comment-display">
-            <p class="comment">Comment</p>
-            <button class="reply-btn">Reply</button>
-            <div class="reply-box"></div>
-        </div>
-    </div>`;
+const messageInput = document.querySelector('.text');
+const reply = document.querySelector('.btn');
+const message = document.querySelector('.msg');
+const comments = document.querySelectorAll('.btn2');
+const sendBtn = document.querySelectorAll('.send');
 
-const ReplyLimit = 4;
-class Comment extends HTMLElement {
-    constructor() {
-        super();
+function Message(){
+    var msg = document.createElement('h1');
+        msg.innerHTML = `${messageInput.value} <button class='btn2' onclick='comment()'>Comment</button>`;
+        message.appendChild(msg)
+        messageInput.value = '';
+}
 
-        this.level = this.getAttribute("level") ? parseInt(this.getAttribute("level")) : 0;
-        this.attachShadow({ mode: "open" });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-        this.commentEdit = this.shadowRoot.querySelector(".comment-edit");
-        this.commentDisplay = this.shadowRoot.querySelector(".comment-display");
-    }
-    connectedCallback() {
-        this.commentEdit.querySelector(".submit-btn").addEventListener("click", () => this.commentSubmit());
-
-        if (this.level < ReplyLimit) {
-            this.commentDisplay.querySelector(".reply-btn").addEventListener("click", () => this.commentReply());
-        } else {
-            this.commentDisplay.querySelector(".reply-btn").disabled = true;
-        }
-        this.commentDisplay.style.display = "none";
-    }
-    commentSubmit() {
-        const commentInput = this.commentEdit.querySelector(".comment-input");
-
-        const comment = this.commentDisplay.querySelector(".comment");
-
-        comment.innerHTML = commentInput.value;
-
-        this.commentEdit.style.display = "none";
-
-        this.commentDisplay.style.display = "block";
-    }
-    commentReply() {
-        const replyBox = this.commentDisplay.querySelector(".reply-box");
-
-        const newCommentBox = document.createElement("comment-box");
-
-        let newLevel = this.level + 1;
-
-        newCommentBox.setAttribute("level", newLevel);
-        newCommentBox.level = newLevel;
-
-        if (replyBox.childNodes) {
-            replyBox.insertBefore(newCommentBox, replyBox.childNodes[0]);
-        } else {
-            replyBox.appendChild(newCommentBox);
-        }
-    }
-    disconnectedCallback() {
-        this.commentEdit.querySelector(".comment-submit").removeEventListener();
-        this.commentDisplay.querySelector(".reply-btn").removeEventListener();
+function click(e) {
+    if (messageInput.value === '') {
+        alert('Cannot send empty message');
+    } else {
+        Message();
     }
 }
 
-window.customElements.define("comment-box", Comment);
+function comment(){
+        var div2 = document.createElement('div');
+        div2.setAttribute('id','input')
+        div2.innerHTML="<input type='text' class='input'> <button class='send' onclick='Send()'>Send</button>";
+        message.appendChild(div2);
+}
+
+function Send(){
+    var commentMsg = document.querySelectorAll('.input');
+    for(var k=0;k<commentMsg.length;k++)
+    var msg2= document.createElement('h1');
+    msg2.innerHTML = `${commentMsg[i].value} <button class='btn2' onclick='comment()'>Comment</button>`;
+    message.appendChild(msg2);
+   
+    document.getElementById('input').style.display="none";
+    console.log('no')
+}
+
+for(var i=0;i<sendBtn.length;i++){
+    sendBtn[i].addEventListener('click',Send);
+    console.log('hi')
+}
+
+for(var j=0;j<comments.length;j++){
+    comments[j].addEventListener('click',comment);
+    console.log('hello')
+}
+
+reply.addEventListener('click',click)
+
